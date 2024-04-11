9d7bb7d9a66a929d425aec59c11f3c3d3ab70e1e
1jds <jamesdstacey2@gmail.com>
2024-04-12 00:44:25 +1000

    added simplified code for problem 15

--- problem-15.js
+++ problem-15.js
@@ -1,50 +1,24 @@
-// Python recursive traversal of tree
-// def treeSum(root):
-//   if root is None:
-//     return 0
-//   else:
-//     leftSum = treeSum(root.left)
-//     rightSum = treeSum(root.right)
-//     return root.data + leftSum + rightSum
-
-class Node {
-  constructor(leaf, gridRefX, gridRefY) {
-    this.leaf = leaf;
-    this.gridRefs = {
-      x: gridRefX,
-      y: gridRefY,
-    };
-    this.left = null;
-    this.right = null;
-  }
-}
-
-let root = new Node(false, 0, 0);
+// Here we get rid of all of the tree stuff, and just do some maths...
+// It works up until a point, when the computer say 'No!'
 
 let count = 0;
 
-function leafCount(currentNode, limit = 2, x = 0, y = 0) {
-  // console.log("iteration... :", currentNode, limit, x, y);
+function leafCount(limit, x = 0, y = 0) {
   if (x === limit && y === limit) {
-    currentNode.leaf = true;
-    currentNode.gridRefs.x = 2;
-    currentNode.gridRefs.y = 2;
     count++;
     return;
   }
   if (y < limit) {
-    let newLeftNode = new Node(false, x, y + 1);
-    currentNode.left = newLeftNode;
-    leafCount(newLeftNode, limit, x, y + 1);
+    leafCount(limit, x, y + 1);
   }
   if (x < limit) {
-    let newRightNode = new Node(false, x + 1, y);
-    currentNode.right = newRightNode;
-    leafCount(newRightNode, limit, x + 1, y);
+    leafCount(limit, x + 1, y);
   }
 }
 
-leafCount(root, 20);
+// leafCount(2);
+// leafCount(4);
+leafCount(9);
+// leafCount(20); Won't work, because it's too many recursive calls...
 
-// console.dir(root, { depth: null });
 console.log(count);
